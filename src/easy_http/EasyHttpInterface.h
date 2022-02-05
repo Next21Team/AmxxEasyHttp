a8ec8838f1e52e65358b34dfc9f377a30689887e
Polarhigh <4231922+Polarhigh@users.noreply.github.com>
2022-02-05 18:05:10 +0300

    compilation fix

--- src/easy_http/EasyHttpInterface.h
+++ src/easy_http/EasyHttpInterface.h
@@ -33,14 +33,14 @@ namespace ezhttp
     {
         struct Progress
         {
-            int32_t download_total{};
-            int32_t download_now{};
-            int32_t upload_total{};
-            int32_t upload_now{};
+            int32_t download_total;
+            int32_t download_now;
+            int32_t upload_total;
+            int32_t upload_now;
         };
 
-        std::atomic_bool canceled;
-        std::atomic<Progress> progress;
+        std::atomic_bool canceled{};
+        std::atomic<Progress> progress{};
     };
 
     class EasyHttpInterface
